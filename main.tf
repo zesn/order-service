5697839dc0f761606d357914efed7545da09055f
zeeahmed2 <zeeahmed2@publicisgroupe.net>
2024-09-30 11:49:17 +0530

    corrected format

--- main.tf
+++ main.tf
@@ -9,9 +9,9 @@ terraform {
   required_version = ">= 1.2.0"
 }
 provider "aws" {
-    region = "${var.region}"
-    access_key = "${var.access_key}"
-    secret_key = "${var.secret_key}"
+  region     = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
 resource "aws_instance" "app_server" {
   ami           = "ami-06f855639265b5541"
--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "region" {
-    description = "AWS Region"
+  description = "AWS Region"
 }
 variable "access_key" {
-    description = "Access Key for AWS"
+  description = "Access Key for AWS"
 }
 variable "secret_key" {
-    description = "Secret_key for AWS"
+  description = "Secret_key for AWS"
 }
